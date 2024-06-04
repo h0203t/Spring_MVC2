9ae1d34b9d6ad7a81ad300c0875dfad550c76442
hyukeh <rnjsgurxo12345@gmail.com>
2024-06-04 21:02:53 +0900

    요구사항 추가

---
+++ form/src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -0,0 +1,12 @@
+package hello.itemservice.domain.item;
+
+public enum ItemType {
+    ;
+
+    private final  String description;
+
+
+    ItemType(String description) {
+        this.description = description;
+    }
+}
